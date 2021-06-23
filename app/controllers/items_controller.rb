b61831bd26708814425043485a3eb623640ef2ad
Katsumoto0127 <nanachan70127@gmail.com>
2021-06-23 12:32:26 +0900

    商品削除機能修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :find_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_index, only: [:edit]
+  before_action :move_index, only: [:edit, :destroy, :update]
 def index
   @items = Item.all.order("created_at DESC")
 end 
